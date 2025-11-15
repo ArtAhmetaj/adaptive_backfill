0955855bb1535024e121c9726db9ceac0629c166
artahmetaj <artahmetaj@yahoo.com>
2025-11-15 10:43:00 +0100

    implement the healthcheck start_link to run once on async cases

--- lib/batch_operation_processor.ex
+++ lib/batch_operation_processor.ex
@@ -9,23 +9,26 @@ defmodule BatchOperationProcessor do
         mode: mode,
         health_checkers: health_checkers
       }) do
-    run_batch_cycle(initial_state, handle_batch, on_complete, mode, health_checkers)
+    health_check_context = init_health_checks(mode, health_checkers)
+    result = run_batch_cycle(initial_state, handle_batch, on_complete, mode, health_check_context)
+    cleanup_health_checks(mode, health_check_context)
+    result
   end
 
-  def run_batch_cycle(state, handle_batch, on_complete, mode, health_checkers) do
+  def run_batch_cycle(state, handle_batch, on_complete, mode, health_check_context) do
     case handle_batch.(state) do
       :done ->
         if on_complete, do: on_complete.(:done)
         {:ok, :done}
 
       {:ok, next_state} ->
-        monitor_results = run_health_checks(mode, health_checkers)
+        monitor_results = run_health_checks(mode, health_check_context)
 
         if MonitorResultEvaluator.halt?(monitor_results) do
           if on_complete, do: on_complete.(next_state)
           {:halt, next_state}
         else
-          run_batch_cycle(next_state, handle_batch, on_complete, mode, health_checkers)
+          run_batch_cycle(next_state, handle_batch, on_complete, mode, health_check_context)
         end
 
       {:error, _reason} = err ->
@@ -33,12 +36,28 @@ defmodule BatchOperationProcessor do
     end
   end
 
-  defp run_health_checks(:async, health_checkers) do
+  defp init_health_checks(:async, health_checkers) do
     {:ok, pid} = AsyncMonitor.start_link(health_checkers)
+    pid
+  end
+
+  defp init_health_checks(:sync, health_checkers) do
+    health_checkers
+  end
+
+  defp run_health_checks(:async, pid) do
     GenServer.call(pid, :get_state)
   end
 
   defp run_health_checks(:sync, health_checkers) do
     SyncMonitor.get_state(health_checkers)
+  end
+
+  defp cleanup_health_checks(:async, pid) do
+    GenServer.stop(pid)
+  end
+
+  defp cleanup_health_checks(:sync, _health_checkers) do
+    :ok
   end
 end
